e5835ca537481daf8935216d0560c6c2c8240622
Max Lewis <max.a.lewis98@gmail.com>
2019-12-14 14:19:05 -0700

    javascript psuedocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// <!-- the hangman game area -->
+//     <!-- wins and loses value -->
+//     <!-- array for potential words -->
+//<!-- display for the potential word all underined untill correct guess -->
+//<!-- remaining guesses value -->
+//<!-- an array that collects old used letters and displayes them -->
+
+
+
+
+var wins = document.getElementById()
+var wins = 0
+
+var loses = document.getElementById()
+var loses = 0
+
+var guesses = document.getElementById()
+var guesses = 15
+
+var wins = document.getElementById()
+var wins = document.getElementById()
