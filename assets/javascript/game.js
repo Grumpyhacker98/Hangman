1ed001b94bb53724d13840a1229012f32db8b9e3
Max Lewis <max.a.lewis98@gmail.com>
2019-12-14 21:21:33 -0700

    more psuedocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,7 @@
 var wins = document.getElementById("#wins")
 var wins = 0
 
-var losses = document.getElementById("#losses")
-var losses = 0
-
-
+var guess = document.getElementById("#guess")
 var guess = 15
 
 var allWords = ["Banana","Orange","Guava","Strawberry"]
@@ -12,20 +9,50 @@ var allWords = ["Banana","Orange","Guava","Strawberry"]
 var chosenWord = document.getElementById("#chosenWord")
 
 var oldGuess = document.getElementById("#oldGuess")
-var oldGuess = []
 
 var start = document.getElementById("#start")
 
-
+// once the page is loaded you can do everything in this
 $(document).ready(function() {
 
+    // (re)start the game, guesses a new word and resets the page
+    // currently broken
     $(start).on("click", function() {
-        chosenWord = Math.random(allWords.length)
+
+        // chose word to start game
+        chosenWord = [Math.floor(Math.random() * allwords.length)]
         console.log(chosenWord)
         targetDiv.textContent = chosenWord;
         $("#start").text(chosenWord)
+
+        // resetpage bit
+
     });
 
-})
+    // something to record keypresses
 
-var guess = document.getElementById("#guess")
+        // if its the right key uncover the hidden word
+        // add the letter to the usedword array
+
+        // if it isnt part of the word then
+        // add the letter to the usedword array
+        // take away one of the guesses left
+
+        // if its a already used word or a unacceptable key give them a warning
+
+
+    // if their avaible guesses go out
+
+        // they get an alert saying they suck
+        // the page resets 
+
+    
+    
+
+
+
+
+
+
+
+})
