e255e3a89abe655a18dca319f55bc937f30cb221
Max Lewis <max.a.lewis98@gmail.com>
2019-12-19 14:27:44 -0700

    still no progress at ALL

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,42 @@
 
-var guess = 0
-var totalGuess = 15
+var guess = 15;
+var totalGuess = 15;
 
-var allWords = ["Banana","Orange","Guava","Strawberry"]
-var chosenWord = [Math.floor(Math.random() * allWords.length)]
+var allWords = ["Banana","Orange","Guava","Strawberry"];
+var chosenWord = [Math.floor(Math.random() * allWords.length)];
+console.log(allWords[chosenWord])
 
 // a function that runs and updates the html with new javascript
 function updateHTML() {
-    wins = document.getElementById("#wins")
-    guess = document.getElementById("#guess")
-    allWords[chosenWord] = document.getElementById("#chosenWord")
-    oldGuess = document.getElementById("#oldGuess")
+    guess = document.getElementById("#guess");
+    allWords[chosenWord] = document.getElementById("#chosenWord");
+    oldGuess = document.getElementById("#oldGuess");
 };
 
 // reset game function run when button clicked or on loss
 function reset() {
-    wins = 0
-    guess = totalGuess
-    chosenWord = [Math.floor(Math.random() * allWords.length)]
-    console.log(allWords[chosenWord])
-    // $("#chosenWord").text(allWords[chosenWord])
-    // $(allWords[chosenWord]).appendto("#chosenWord")
-    // allWords[chosenWord] = document.getElementById("#chosenWord")
-}
+    guess = totalGuess;
+    chosenWord = [Math.floor(Math.random() * allWords.length)];
+    console.log(allWords[chosenWord]);
+    // $("#chosenWord").text(allWords[chosenWord]);
+    // $(allWords[chosenWord]).appendto("#chosenWord");
+    // allWords[chosenWord] = document.getElementById("#chosenWord");
+};
 
 // once the page is loaded you can do everything in this
 $(document).ready(function() {
 
+    updateHTML()
     // (re)start the game, guesses a new word and resets the page
     $("#start").on("click", function() {
 
-        reset()
-        updateHTML()
-        // // chose word to start game
-        // chosenWord = [Math.floor(Math.random() * allWords.length)]
-        // console.log(chosenWord)
-        // $("#chosenWord").textContent = chosenWord;
-        // // $("#start").text(chosenWord)
+        guess--;
+        console.log(guess);
 
+        // reset()
+        // updateHTML()
+
+    
     });
 
     // something to record keypresses
@@ -56,17 +55,8 @@ $(document).ready(function() {
     // the defeat if statement
     if ( guess = 0 ) {
         alert("You Lost!")
-        reset()
         updateHTML()
-        // reset function needed
-    }
+        // fully reveal the word
+    };
     
-    
-
-
-
-
-
-
-
-})
+});
