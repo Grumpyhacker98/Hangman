c85a83014388b94d1e20613fa12b03e89fdc45c6
Max Lewis <max.a.lewis98@gmail.com>
2019-12-19 15:44:03 -0700

    restarted script compleatly to try again, word generation and guesses work but nothing else

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,48 @@
 
+// guess value and value used to restart the game along with initial display command
 var guess = 15;
 var totalGuess = 15;
+$("#guess").text(guess);
+
 
 var allWords = ["Banana","Orange","Guava","Strawberry"];
-var chosenWord = [Math.floor(Math.random() * allWords.length)];
-console.log(allWords[chosenWord])
 
-// a function that runs and updates the html with new javascript
+// keeps html updated
 function updateHTML() {
-    guess = document.getElementById("#guess");
-    allWords[chosenWord] = document.getElementById("#chosenWord");
-    oldGuess = document.getElementById("#oldGuess");
+    
+    $("#guess").text(guess);
+
 };
 
-// reset game function run when button clicked or on loss
-function reset() {
-    guess = totalGuess;
-    chosenWord = [Math.floor(Math.random() * allWords.length)];
+// restarts Gamepad, having issues chosing new word it was working earlier
+function reStart(){
+    guess = totalGuess
+    var chosenWord = [Math.floor(Math.random() * allWords.length)];
+    $("#chosenWord").replaceWith(allWords[chosenWord]);
     console.log(allWords[chosenWord]);
-    // $("#chosenWord").text(allWords[chosenWord]);
-    // $(allWords[chosenWord]).appendto("#chosenWord");
-    // allWords[chosenWord] = document.getElementById("#chosenWord");
 };
 
-// once the page is loaded you can do everything in this
+
 $(document).ready(function() {
 
-    updateHTML()
-    // (re)start the game, guesses a new word and resets the page
+    // (re)start button
     $("#start").on("click", function() {
+        reStart()
+        updateHTML();
+    });
+
+    // when key is pressed most logic happens
+    document.onkeyup = function(){
 
         guess--;
         console.log(guess);
 
-        // reset()
-        // updateHTML()
+        // loss statement
+        if (guess === 0){
+            alert("you lose!")
+        }
 
-    
-    });
+        updateHTML();
 
-    // something to record keypresses
-
-        // if its the right key uncover the hidden word
-        // add the letter to the usedword array
-
-        // if it isnt part of the word then
-        // add the letter to the usedword array
-        // take away one of the guesses left
-
-        // if its a already used word or a unacceptable key give them a warning
-
-
-
-    // the defeat if statement
-    if ( guess = 0 ) {
-        alert("You Lost!")
-        updateHTML()
-        // fully reveal the word
     };
-    
 });
