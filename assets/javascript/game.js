a22938588cebec14929b849b1a9fbffd6fb5c760
Max Lewis <max.a.lewis98@gmail.com>
2020-04-06 15:45:29 -0700

    cant use require as its node so its one file, works on console.log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,218 +1,163 @@
+// constructors
+// =====================================================================================================
+// gives every letter variables and for loops to collect it all again and display the users progress
+function newLetter(char) {
 
-var guess = 15;
-var totalGuess = 15;
+    this.char = char
+    this.bool = false
 
-var allWords = ["Banana","Orange","Guava","Strawberry"];
-var chosenWord = [];
+    // if the letter is a space make .bool true as its not a letter to guess
+    if (this.char === " ") {
+        this.bool = true
+    }
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var oldGuess = [];
+    // pushes letters to printArr so printArr can print the whole word partially revealed
+    this.printLetter = function () {
+        if (this.bool) {
+            printArr.push(this.char)
+        } else {
+            printArr.push("_")
+        }
+    }
 
-// keeps html updated
-function updateHTML() {
-    
-    $("#guess").text(guess);
-    $("#oldGuess").text(oldGuess);
+    this.check = function (arg) {
+        if (this.char === arg) {
+            this.bool = true
+            // return true so that the convertToWord.guess can return true so that it can print an incorrect or correct statement
+            return true
+        }
+    }
+}
+// for loops for print functions
+// will turn into return variable to print unto screen properly
+function ConvertToPuzzle(word) {
 
-};
+    // create letter constructor array
+    this.word = word
+    this.array = []
+    wordArr = word.split("")
+    for (var i = 0; i < wordArr.length; i++) {
+        letter = new newLetter(wordArr[i])
+        this.array.push(letter)
+    }
 
+    // create and print printArr of covered/uncovered letters with dividers
+    this.print = function () {
+        printArr = []
+        for (var i = 0; i < this.array.length; i++) {
+            this.array[i].printLetter()
+        }
+        newPrint = printArr.join("|")
+        console.log("|" + newPrint + "|")
+    }
 
-$(document).ready(function() {
+    // looks through every letter to see if the guess is correct
+    // needs to return trueGuess instead of straight true value becouse if its in the if statement it wont reveal multi char correct awnsers
+    this.guess = function (arg) {
+        trueGuess = false
+        for (var i = 0; i < this.array.length; i++) {
+            if (this.array[i].check(arg)) {
+                trueGuess = true
+            }
+        }
+        return trueGuess
+    }
 
-    // (re)start button
-    $("#start").on("click", function() {
-        // other reset stuff
-        guess = totalGuess;
-        oldGuess = [];
-        
-        // guesses new word
-        chosenWord = [];
-        var wordChoice = [Math.floor(Math.random() * allWords.length)];
-        chosenWord.push(allWords[wordChoice]);
-        $("#chosenWord").text(chosenWord);
-
-        updateHTML();
-    });
-
-    // when key is pressed most logic happens
-    document.onkeyup = function(event){
-        var currentGuess = event.key;
-
-
-
-        // experimental code
-
-        // loop through the word and check every character
-        for (var i=0, j=chosenWord.length; i<j; i++) {
-
-            if (true){
-                console.log("placeholder so guess attempts work")
-                guess--;
-                oldGuess.push(currentGuess);
+    // if every letter.boolien value is true retun true so hangman.js can run congrats victory code
+    this.winCon = function () {
+        winScore = 0
+        for (var i = 0; i < this.array.length; i++) {
+            if (this.array[i].bool) {
+                winScore++
             }
 
-            // correct guess if statement is just a congrats alert right now
-            if (currentGuess == chosenWord[i]){
-                console.log("you got this one right!");
-                guess--;
-                oldGuess.push(currentGuess);
-            };
+        }
+        if (winScore === this.array.length) {
+            return true
+        }
+    }
 
-            // only alphabet characters are accepted
-            //  might need to nest the entire for loop in this
-            if(!currentGuess == letters){
-                console.log("thats not part of the alphabet!");
-            };
+}
 
-            // ensures you dont press the same key twice
-            if (currentGuess == oldGuess){
-                console.log("you pressed the "+currentGuess+" key twice!");
-            };
-
-            // wrong guess if statement
-            if (!currentGuess === chosenWord[i]){
-                console.log("wrong!");
-                guess--;
-                oldGuess.push(currentGuess);
-            };
-            
-        };
-
-        
-
-
-        // experimental code ends
-
-
-
-        // a victory condition somehow
-
-        // loss statement
-        if (guess === 0){
-            alert("you lose!");
-        };
-
-        updateHTML();
-
-    };
-});
-
-
-// =====================================================================
-// =====================================================================
-
-const inquirer = require('inquirer');
-
-var ConvertToPuzzle = require("./constructors.js");
+// primary game frame
+// ==============================================================
 
 // word bank
-var words = ["conundrum","implication","onamonapia"]
+var words = ["conundrum", "implication", "onamonapia"]
+var guessArr = [];
+
+var gamestart = false
 
 const maxCount = 7
 
 
-// (re)start game prompt
-function startGame(){
-    console.log("=================================")
-    inquirer.prompt([
-        {
-            type: "confirm",
-            message: "Do you wish to play a game of hangman?",
-            name: "data"
-        }
-    ]).then(function (response) {
-        // setup game
-        if(response.data){
-            // generate new word constructor from word array index
-            word = new ConvertToPuzzle(words[Math.floor(Math.random() * 3)])
-            count = maxCount
-            guessArr = []
-            console.log("you can type 'exit' to leave")
-            console.log("=================================")
-            word.print()
-            console.log("=================================")
-            askQuestion()
-        }else{
-            // exit program
-            console.log("take care!")
-            console.log("=================================")
-            return false;
-        }
-    });
-}
+$(document).ready(function () {
 
-// game cycle, asks questions until counter variable is 0
-function askQuestion() {
+    // (re)start button
+    $("#start").on("click", function () {
 
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Guess a letter >",
-            name: "data"
-        }
-    ]).then(function (response) {
-        guess = response.data.toLowerCase()
-        
-        // to leave at any time
-        if(guess==="exit"){
-            console.log("take care!")
-            console.log("=================================")
-            return false;
-        }
-        
-        // reasks question if guess is longer than 1 character or non alphabet or empty
-        if(guess.length>1||!/^[a-z]*$/g.test(guess)||guess===" "){
-            console.log("unacceptable guess")
-            console.log("=================================")
-            askQuestion()
-            return false;
-        }
-
-        // checks to see if letter has been guessed b4 to prevent unneccisary counter losses
-        for(var i=0;i<guessArr.length;i++){
-            if(guess===guessArr[i]){
-                console.log("You have guessed this choice before")
-                console.log("=================================")
-                askQuestion()
-                return false;
-            }
-        }
-
-        // runs guess function if it returns true even once it will console log Correct statement
-        if(word.guess(guess)){
-            console.log("Correct")
-        }else{
-            console.log("Incorrect")
-            guessArr.push(guess)
-            count--;
-        }
-
-        console.log("Wrong tries: "+guessArr)
+        gameStart = true
+        word = new ConvertToPuzzle(words[Math.floor(Math.random() * 3)])
+        count = maxCount
+        guessArr = []
         word.print()
 
-        // if all letter.bool values are true then it will return true and run victory
-        if(word.winCon()){
-            console.log("=================================")
-            console.log("Congratulations! You Won!")
-            console.log("The word was: "+word.word)
-            startGame()
+    });
+
+    // when key is pressed it is lowercased and parsed  into gamecycle
+    document.onkeyup = function (event) {
+        if(gameStart){
+            askQuestion(event.key.toLowerCase())
+        }
+    };
+
+})
+
+// game cycle
+// ===============================================================================
+function askQuestion(guess) {
+
+    // reasks question if guess is longer than 1 character or non alphabet or empty
+    if (guess.length > 1 || !/^[a-z]*$/g.test(guess) || guess === " ") {
+        console.log("unacceptable guess")
+        return false;
+    }
+
+    // checks to see if letter has been guessed b4 to prevent unneccisary counter losses
+    for (var i = 0; i < guessArr.length; i++) {
+        if (guess === guessArr[i]) {
+            console.log("You have guessed this choice before")
             return false;
         }
+    }
 
-        console.log("Attempts left: "+count)
+    // use constructor to find true false on guess
+    if (word.guess(guess)) {
+        console.log("Correct")
+    } else {
+        console.log("Incorrect")
+        guessArr.push(guess)
+        count--;
+    }
+
+    // if all letter.bool values are true then it will return true and run victory
+    if (word.winCon()) {
         console.log("=================================")
+        console.log("Congratulations! You Won!")
+        console.log("The word was: " + word.word)
+        console.log("=================================")
+        // startGame()
+        return false;
+    }
 
-        // reAsk askquestion()
-        if(count>0){
-            askQuestion()
-        }else{
-            console.log("Sorry, but you lost!")
-            console.log("The word was: "+word.word)
-            startGame()
-        }
-
-    });
+    // countdown
+    if (count > 0) {
+        console.log("Wrong tries: " + guessArr)
+        word.print()    
+        console.log("Attempts left: " + count)
+    } else {
+        console.log("Sorry, but you lost!")
+        console.log("The word was: " + word.word)
+        gameStart = false
+    }
 }
-
-// nuff said
-startGame()
